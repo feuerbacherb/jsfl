1615e7726c9866ec1caf1c5c06be09cccc1207c7
Brent Feuerbacher <feuerbacherb@gmail.com>
2025-08-20 08:00:04 -0500

    commiting to close test pages

--- test2.js
+++ test2.js
@@ -5,32 +5,41 @@
     let strURL = "";
 
     let strBI = document.getElementById(div1).style.backgroundImage.slice(4, -1).replace(/["'"]/g, "");
-    //console.log(`strBI = ${strBI}`);
+    console.log(`strBI = ${strBI}`);
     let arrBI = strBI.split(".");
-    //printArray(arrBI);
+    printArray(arrBI);
 
-    arrBI[4] = color;
+    arrBI[3] = color;
+    printArray(arrBI);
     strURL = concatArray(arrBI);
-    //console.log(`strURL = ${strURL}`);
+    console.log(`strURL = ${strURL}`);
 
     document.getElementById(div1).style.backgroundImage = "url(" + strURL + ")";
     document.getElementById(div2).style.backgroundImage = "url(" + strURL + ")";
   }
 
-function printArray(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(`arrBI[${i}] = ${arr[i]}`);
-  };
-}
+  function printArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(`arrBI[${i}] = ${arr[i]}`);
+    };
+  }
 
 function concatArray(arr) {
   let strArr = "";
   for (let i = 0; i < arr.length; i++) {
     strArr += arr[i];
-    if (i != arr.length - 1) {
+    if (i != arr.length) {
       strArr += ".";
-    }
+    }    
   }
+  //strArr += ".png";
   return strArr;
 }
+
+$(function() {
+  $('#s-toggle-event').change(function() {
+    $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+  })
+})
+
 </script>
